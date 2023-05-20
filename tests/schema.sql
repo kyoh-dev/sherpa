0fc1f78f0d8ec0b419c40a213b42b0f8ef1aad5a
Connor <contact@kyoh.run>
2023-05-20 15:50:09 +1000

    Remove unnecessary schema.sql file from test

--- tests/schema.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE public.geometry_load_test (
-    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    polygon_id TEXT NOT NULL,
-    geometry GEOMETRY(Polygon, 4326) NOT NULL
-);
-
-CREATE SCHEMA generic;
